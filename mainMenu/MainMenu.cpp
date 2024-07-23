9d196cbc34e5a759eaf3d7b7cc3b622ee7916fd9
DominkoMilic <147631112+DominkoMilic@users.noreply.github.com>
2024-07-23 17:08:05 +0200

    mainmenu class

--- mainMenu/MainMenu.cpp
+++ mainMenu/MainMenu.cpp
@@ -1,1 +1,113 @@
 #include "MainMenu.h"
+
+MainMenu::MainMenu(){
+    for (int i = 0; i < allImagesMatrix.size(); ++i) {
+        allTexturesMatrix[i].resize(allImagesMatrix[i].size());
+        for (size_t j = 0; j < allImagesMatrix[i].size(); ++j) {
+            if (!allTexturesMatrix[i][j].loadFromFile(allImagesMatrix[i][j])) {
+                std::cerr << "Failed to load " << allImagesMatrix[i][j] << std::endl;
+            }
+        }
+    }
+}
+
+void MainMenu::imagesReader(std::vector<std::vector<std::string>>& allImagesMatrix){
+        std::ifstream file("imagesPaths.txt");
+    if (!file.is_open())
+    {
+        std::cerr << "Unable to open imagesPaths.txt" << std::endl;
+        return;
+    }
+    std::vector<std::string> currentRow;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (isNumber(line)) {
+            if (!currentRow.empty()) {
+                allImagesMatrix.push_back(currentRow);
+                currentRow.clear();
+            }
+            currentRow.push_back(line);
+        }
+        else {
+            currentRow.push_back(line);
+        }
+    }
+    if (!currentRow.empty()) {
+        allImagesMatrix.push_back(currentRow);
+    }
+
+    file.close();
+}
+
+bool MainMenu::isNumber(std::string& line) {
+    for (char c : line) {
+        if (!isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void MainMenu::enemyStatsReader(std::vector<std::vector<int>>& enemyStatsMatrix){
+    std::ifstream file("enemyStats.txt");
+    if (!file.is_open())
+    {
+        std::cerr << "Unable to open enemyStats.txt" << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        std::vector<int> currentRow;
+        std::string value;
+        while (iss >> value) {
+            if (isNumber(value)) {
+                currentRow.push_back(std::stoi(value));
+            }
+            else {
+                std::cerr << "Invalid number: " << value << std::endl;
+                return;
+            }
+        }
+        if (!currentRow.empty()) {
+            enemyStatsMatrix.push_back(currentRow);
+        }
+    }
+
+    file.close();
+}
+
+
+void MainMenu::towerStatsReader(std::vector<std::vector<int>>& towerStatsMatrix) {
+    std::ifstream file("tower_stats.txt");
+    if (!file.is_open())
+    {
+        std::cerr << "Unable to open tower_stats.txt" << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        std::vector<int> currentRow;
+        std::string value;
+        while (iss >> value) {
+            if (isNumber(value)) {
+                currentRow.push_back(std::stoi(value));
+            }
+            else {
+                std::cerr << "Invalid number: " << value << std::endl;
+                return;
+            }
+        }
+        if (!currentRow.empty()) {
+            towerStatsMatrix.push_back(currentRow);
+        }
+    }
+
+    file.close();
+}
--- mainMenu/MainMenu.h
+++ mainMenu/MainMenu.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
 class MainMenu
 {
+private:
+    std::vector<std::vector<std::string>> allImagesMatrix;
+    std::vector<std::vector<int>> enemyStatsMatrix; 
+    std::vector<std::vector<int>> towerStatsMatrix;
+    std::vector<std::vector<sf::Texture>> allTexturesMatrix;
+public:
+    void imagesReader(std::vector<std::vector<std::string>>& allImagesMatrix);
+    void enemyStatsReader(std::vector<std::vector<int>>& enemyStatsMatrix);
+    void towerStatsReader(std::vector<std::vector<int>>& towerStatsMatrix);
+    bool isNumber(std::string& line);
+    void textureMatrixFiller();
 };
